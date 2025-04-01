88a655e1fd650d8fb6ff7084ffa230a88cb29501
djoek <djoek@users.noreply.github.com>
2025-04-01 09:28:07 +0200

    Reorganisation

--- 1_lists_slices_sets/list_sum_set_range.py
+++ 1_lists_slices_sets/list_sum_set_range.py
@@ -3,10 +3,10 @@ rest = set(range(1, 101)) - set(fizz) - set(buzz)
 
 
 def fizzbuzz(i: int) -> str:
-    return  str(["FizzBuzz"][:i in fizz and i in buzz] or
-                ["Fizz"][:i in fizz] or
-                ["Buzz"][:i in buzz] or
-                [i][:i in rest])
+    return str(["FizzBuzz"][:i in fizz and i in buzz] or
+               ["Fizz"][:i in fizz] or
+               ["Buzz"][:i in buzz] or
+               [i][:i in rest])
 
 
 print(*map(fizzbuzz, range(1, 101)), sep="\n")
